bf9a8f78b17ad412a1957f3f469cd579f1d67cef
kindainoob <40814048+kindainoob@users.noreply.github.com>
2018-09-21 01:36:48 +0900

    コメントの追記

--- command.py
+++ command.py
@@ -9,6 +9,7 @@ cut_file = file.split('.')
 # 各，拡張子をつける
 platex = 'platex ' + cut_file[0] + '.tex'
 dvipdfmx = 'dvipdfmx ' + cut_file[0] + '.dvi'
+# ログファイルの削除
 clean = 'rm '+ cut_file[0] +'.dvi '+ cut_file[0] +'.log '+ cut_file[0] +'.aux'
 # 実行
 subprocess.call(platex.split())
