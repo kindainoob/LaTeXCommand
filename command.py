1c866b7dd04bf9f6f4b3c7badb82fedfb9ba68e9
kindainoob <40814048+kindainoob@users.noreply.github.com>
2018-09-21 01:34:18 +0900

    Update command.py
    completed

--- command.py
+++ command.py
@@ -2,14 +2,15 @@
 import subprocess
 import sys
 args = sys.argv
-
+# 引数を変数に取得
 file = args[1]
-
+# 拡張子を切り捨て
 cut_file = file.split('.')
+# 各，拡張子をつける
 platex = 'platex ' + cut_file[0] + '.tex'
 dvipdfmx = 'dvipdfmx ' + cut_file[0] + '.dvi'
 clean = 'rm '+ cut_file[0] +'.dvi '+ cut_file[0] +'.log '+ cut_file[0] +'.aux'
-
+# 実行
 subprocess.call(platex.split())
 subprocess.call(platex.split())
 subprocess.call(dvipdfmx.split())
